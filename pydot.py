3d22365251b5a13d02a1203dc6909fded0cb13a3
Krzysztof Baranski <baranski5@gmail.com>
2016-05-07 20:07:27 +0200

    python3 compatibility

--- pydot.py
+++ pydot.py
@@ -29,8 +29,8 @@ import tempfile
 import copy
 try:
     import dot_parser
-except Exception, e:
-    print "Couldn't import dot_parser, loading of dot files will not be possible."
+except Exception as e:
+    print("Couldn't import dot_parser, loading of dot files will not be possible.")
     
 
 
@@ -92,7 +92,7 @@ CLUSTER_ATTRIBUTES = set( ['K', 'URL', 'bgcolor', 'color', 'colorscheme',
 #
 class frozendict(dict):
     def _blocked_attribute(obj):
-        raise AttributeError, "A frozendict cannot be modified."
+        raise (AttributeError, "A frozendict cannot be modified.")
     _blocked_attribute = property(_blocked_attribute)
 
     __delitem__ = __setitem__ = clear = _blocked_attribute
@@ -506,7 +506,7 @@ def find_graphviz():
                             #print "Used Windows registry"
                             return progs
                 
-                except Exception, excp:
+                except Exception as excp:
                     #raise excp
                     pass
                 else:
@@ -717,7 +717,8 @@ class InvocationException(Exception):
 
 
 
-class Node(object, Common):
+class Node(Common):
+
     """A graph node.
     
     This class represents a graph's node with all its attributes.
@@ -834,7 +835,8 @@ class Node(object, Common):
 
 
 
-class Edge(object,  Common ):
+class Edge(Common ):
+
     """A graph edge.
     
     This class represents a graph's edge with all its attributes.
@@ -925,7 +927,7 @@ class Edge(object,  Common ):
         """
         
         if not isinstance(edge, Edge):
-            raise Error, "Can't compare and edge to a non-edge object."
+            raise(Error, "Can't compare and edge to a non-edge object.")
             
         if self.get_parent_graph().get_top_graph_type() == 'graph':
         
@@ -1025,7 +1027,7 @@ class Edge(object,  Common ):
     
     
     
-class Graph(object, Common):
+class Graph(Common):
     """Class representing a graph in Graphviz's dot language.
 
     This class implements the methods to work on a representation
@@ -1075,7 +1077,7 @@ class Graph(object, Common):
             self.obj_dict['attributes'] = dict(attrs)
             
             if graph_type not in ['graph', 'digraph']:
-                raise Error, 'Invalid type "%s". Accepted graph types are: graph, digraph, subgraph' % graph_type
+                raise(Error, 'Invalid type "%s". Accepted graph types are: graph, digraph, subgraph' % graph_type)
     
     
             self.obj_dict['name'] = quote_if_necessary(graph_name)
@@ -1270,7 +1272,7 @@ class Graph(object, Common):
         """
         
         if not isinstance(graph_node, Node):
-            raise TypeError('add_node() received a non node class object: ' + str(graph_node))
+            raise(TypeError('add_node() received a non node class object: ' + str(graph_node)))
 
             
         node = self.get_node(graph_node.get_name())
@@ -1373,7 +1375,7 @@ class Graph(object, Common):
         """
 
         if not isinstance(graph_edge, Edge):
-            raise TypeError('add_edge() received a non edge class object: ' + str(graph_edge))
+            raise(TypeError('add_edge() received a non edge class object: ' + str(graph_edge)))
 
         edge_points = ( graph_edge.get_source(), graph_edge.get_destination() )
 
@@ -1496,7 +1498,7 @@ class Graph(object, Common):
         """
 
         if not isinstance(sgraph, Subgraph) and not isinstance(sgraph, Cluster):
-            raise TypeError('add_subgraph() received a non subgraph class object:' + str(sgraph))
+            raise(TypeError('add_subgraph() received a non subgraph class object:' + str(sgraph)))
             
         if self.obj_dict['subgraphs'].has_key(sgraph.get_name()):
         
@@ -1949,16 +1951,16 @@ class Dot(Graph):
         if self.progs is None:
             self.progs = find_graphviz()
             if self.progs is None:
-                raise InvocationException(
-                    'GraphViz\'s executables not found' )
+                raise(InvocationException(
+                    'GraphViz\'s executables not found' ))
                 
         if not self.progs.has_key(prog):
-            raise InvocationException(
-                'GraphViz\'s executable "%s" not found' % prog )
+            raise(InvocationException(
+                'GraphViz\'s executable "%s" not found' % prog ))
             
         if not os.path.exists( self.progs[prog] ) or not os.path.isfile( self.progs[prog] ):
-            raise InvocationException(
-                'GraphViz\'s executable "%s" is not a file or doesn\'t exist' % self.progs[prog] )
+            raise(InvocationException(
+                'GraphViz\'s executable "%s" is not a file or doesn\'t exist' % self.progs[prog] ))
             
             
         tmp_fd, tmp_name = tempfile.mkstemp()
@@ -2018,11 +2020,11 @@ class Dot(Graph):
         status = p.wait()
         
         if status != 0 :
-            raise InvocationException(
+            raise(InvocationException(
                 'Program terminated with status: %d. stderr follows: %s' % (
-                    status, stderr_output) )
+                    status, stderr_output) ))
         elif stderr_output:
-            print stderr_output
+            print(stderr_output)
         
         # For each of the image files...
         #
